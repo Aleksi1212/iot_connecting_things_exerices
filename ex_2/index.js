0cb213a9412c1c6e28d711c9dc5bd206fe385dfc
Aleksi Noro <aleksinoro@gmail.com>
2025-10-31 21:39:23 +0200

    ex_2

--- ex_2/index.js
+++ ex_2/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mqtt = require('mqtt');
+const nanoid = require('nanoid');
 const path = require('path');
 const util = require('util');
 const fs = require('fs');
@@ -11,6 +12,8 @@ const PORT = 3000;
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 // Create variables for MQTT use here
+const mqttBroker = "192.168.50.111"
+const mqttPort = 1883
 
 app.use(bodyParser.json());
 function read(filePath = './message.json') {
@@ -23,26 +26,111 @@ function write(data, filePath = './message.json') {
 }
 
 // create an MQTT instance
-
+let client = mqtt.connect(`mqtt://${mqttBroker}:${mqttPort}`)
 
 // Check that you are connected to MQTT and subscribe to a topic (connect event)
+client?.on("connect", async () => {
+    const topics = await read("./topics.json")
+    for (let i = 0; i < topics.length; i++) {
+        const { name } = topics[i];
+        client.subscribe(name, (err) => {
+            if (!err) {
+                client.publish(name, `Hello ${name}!`);
+                console.log(`Connected to topic: ${name}`)
+            } else {
+                console.log(`Error connecting to topic: ${name}`)
+            }
+        })
+    }
+    // for (let i = 0; i < mqttSendTopic.length; i++) {
+    //     const topic = mqttSendTopic[i]
+    //     client.subscribe(topic, (err) => {
+    //         if (!err) {
+    //             client.publish(topic, `Hello ${topic}`)
+    //             console.log(`Connected to send topic: ${topic}`)
+    //         }
+    //     })
+    // }
+    // for (let i = 0; i < mqttReadTopic.length; i++) {
+    //     const topic = mqttReadTopic[i]
+    //     client.subscribe(topic, (err) => {
+    //         if (!err) {
+    //             // client.publish(topic, `Hello ${topic}`)
+    //             console.log(`Connected to read topic: ${topic}`)
+    //         }
+    //     })
+    // }
+})
 
 // handle instance where MQTT will not connect (error event)
+client?.on("error", () => {
+    console.log("error with mqtt")
+    client = null
+})
 
 // Handle when a subscribed message comes in (message event)
+client?.on("message", async (topic, message) => {
+    // if (mqttReadTopic.includes(topic)) {
+    let jsonData = await read()
+    jsonData.push({ "id": nanoid.nanoid(10), "topic": topic, "msg": message.toString() })
+    write(jsonData).then(() => console.log("success")).catch((err) => console.log(err))
+    // }
+})
 
 // Route to serve the home page
 app.get('/', (_, res) => {
     res.sendFile(path.resolve(__dirname, './index.html'));
 });
 
+
 // route to serve the JSON array from the file message.json when requested from the home page
+app.get('/messages', async (req, res) => {
+    const jsonData = await read()
+    res.json(jsonData)
+})
 
 // Route to serve the page to add a message
+app.get('/add', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./message.html"))
+})
 
 //Route to show a selected message. Note, it will only show the message as text. No html needed
+app.get('/:id', async (req, res) => {
+    const jsonData = await read()
+    const msg = jsonData.find((item) => item["id"] == req.params.id)
+    res.json(msg)
+})
 
 // Route to CREATE a new message on the server and publish to mqtt broker
+app.post('/', async (req, res) => {
+    try {
+        const { topic, msg } = req.body;
+        let topics = await read("./topics.json")
+        const foundTopic = topics.find((topicObj) => topicObj["name"] == topic)
+        if (foundTopic && foundTopic["send"]) {
+            client.publish(foundTopic["name"], msg)
+            res.sendStatus(200)
+        } else if (foundTopic && !foundTopic["send"]) {
+            res.sendStatus(401)
+        } else {
+            client.subscribe(topic, (err) => {
+                if (!err) {
+                    topics.push({ name: topic, send: true })
+                    write(topics, "./topics.json")
+                    
+                    client.publish(topic, msg)
+                    console.log(`Connected to topic: ${topic}`)
+                    res.sendStatus(200)
+                } else {
+                    console.log(`Error connecting to topic: ${topic}`)
+                    res.sendStatus(500)
+                }
+            })
+        }
+    } catch {
+        res.sendStatus(500)
+    }
+})
 
 // Route to delete a message by id (Already done for you)
 
